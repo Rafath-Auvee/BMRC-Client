a965584f0f277da191bc7e001d29d115a724938e
Rafath-Bin-Zafar-Auvee <rbza98@outlook.com>
2022-08-15 15:49:14 +0600

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/shared/Navbar";
+import Home from "./components/pages/Home/Home";
+
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
